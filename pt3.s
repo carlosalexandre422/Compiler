507b5c5116558e053983a5befb05427b3ce22cef
carlosalexandre422 <carlos@DESKTOP-U1CNAR1.>
2024-12-20 17:51:02 -0300

    atvdd 3

--- pt3.s
+++ pt3.s
@@ -4,15 +4,15 @@
 
 _start:
     # (512 * 65)
-    mov $5657, %rax
-    imul $23, %rax
+    mov $512, %rax
+    imul $65, %rax
 
     # Salva resultado parcial em RBX
     mov %rax, %rbx
 
     # (5657 * 23)
-    mov $512, %rax
-    imul $65, %rax
+    mov $5657, %rax
+    imul $23, %rax
 
     # Subtração
     sub %rax, %rbx
@@ -20,5 +20,6 @@ _start:
     # Move resultado final para RAX
     mov %rbx, %rax
 
+    # vai retornar um erro "Segmentation fault (core dumped)" pois o resultado é um número negativo
     call imprime_num
     call sair
